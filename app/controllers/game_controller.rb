b196804d7aa208ce9be6c93db470aafcc31c0674
Nicole Mcinerney <nicolemci@hotmail.com>
2024-07-13 08:43:53 +0100

    added home links to home page and new view

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,14 +1,13 @@
 # app/controllers/game_controller.rb
 class GameController < ApplicationController
-  def index
+  def population
     @countries = CountryPopulation.order('RANDOM()').limit(10)
     @turns_left = 5
   end
 
-  def check_order
+  def check_population_order
     selected_country_ids = params[:countries]
 
-    # Ensure selected_country_ids is an array if it's coming as a string
     selected_country_ids = selected_country_ids.is_a?(String) ? selected_country_ids.split(',') : selected_country_ids
 
     @correct = selected_country_ids.each_cons(2).all? do |a, b|
@@ -19,13 +18,69 @@ class GameController < ApplicationController
 
     if @correct
       flash[:notice] = "Correct! You have successfully ranked the countries by population."
-      redirect_to root_path
+      redirect_to games_population_path
     elsif @turns_left.zero?
       flash[:alert] = "Game over! You have used all your turns."
-      redirect_to root_path
+      redirect_to games_population_path
     else
       @countries = CountryPopulation.find(selected_country_ids)
-      render :index
+      render :population
+    end
+  end
+
+  def area
+    @countries = CountryArea.order('RANDOM()').limit(10)
+    @turns_left = 5
+  end
+
+  def check_area_order
+    selected_country_ids = params[:countries]
+
+    selected_country_ids = selected_country_ids.is_a?(String) ? selected_country_ids.split(',') : selected_country_ids
+
+    @correct = selected_country_ids.each_cons(2).all? do |a, b|
+      CountryArea.find(a).area <= CountryArea.find(b).area
+    end
+
+    @turns_left = params[:turns_left].to_i - 1
+
+    if @correct
+      flash[:notice] = "Correct! You have successfully ranked the countries by area."
+      redirect_to games_area_path
+    elsif @turns_left.zero?
+      flash[:alert] = "Game over! You have used all your turns."
+      redirect_to games_area_path
+    else
+      @countries = CountryArea.find(selected_country_ids)
+      render :area
+    end
+  end
+
+  def capital
+    @countries = CountryCapital.order('RANDOM()').limit(10)
+    @turns_left = 5
+  end
+
+  def check_capital_order
+    selected_country_ids = params[:countries]
+
+    selected_country_ids = selected_country_ids.is_a?(String) ? selected_country_ids.split(',') : selected_country_ids
+
+    @correct = selected_country_ids.each_cons(2).all? do |a, b|
+      CountryCapital.find(a).capital <= CountryCapital.find(b).capital
+    end
+
+    @turns_left = params[:turns_left].to_i - 1
+
+    if @correct
+      flash[:notice] = "Correct! You have successfully ranked the countries by capital cities."
+      redirect_to games_capital_path
+    elsif @turns_left.zero?
+      flash[:alert] = "Game over! You have used all your turns."
+      redirect_to games_capital_path
+    else
+      @countries = CountryCapital.find(selected_country_ids)
+      render :capital
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  root 'game#index'
-  post 'check_order', to: 'game#check_order'
+  root 'home#index'
+
+  get 'games/population', to: 'game#population'
+  post 'games/population/check_order', to: 'game#check_population_order', as: 'check_population_order'
+
+  get 'games/area', to: 'game#area'
+  post 'games/area/check_order', to: 'game#check_area_order', as: 'check_area_order'
+
+  get 'games/capital', to: 'game#capital'
+  post 'games/capital/check_order', to: 'game#check_capital_order', as: 'check_capital_order'
+
+  # Add more routes for additional games as needed
 end
